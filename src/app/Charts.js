2862ba5c89dcc1c2058e6c5a31bb52b849892c40
Alfredo Skerl <alfredos@oktana.io>
2016-10-18 12:36:24 -0300

    Add panel component to wrap sections

--- src/app/Charts.js
+++ src/app/Charts.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import DonutChart from './DonutChart';
+import Panel from './Panel';
 
 class Charts extends React.Component {
 
@@ -22,40 +23,26 @@ class Charts extends React.Component {
       <div>
         <div className="row">
           <div className="col-lg-12">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">
-                  {`Risk profile: ${this.state.currentChart}`}
-                </h3>
-              </div>
-              <div className="panel-body">
-                <input
-                  id="risk"
-                  ref={(c) => this._input = c}
-                  type="range"
-                  min="1"
-                  max="10"
-                  step="1"
-                  defaultValue="1"
-                  onChange={this._handleOnChange.bind(this)}
-                />
-              </div>
-            </div>
+            <Panel title={`Risk profile: ${this.state.currentChart}`}>
+              <input
+                id="risk"
+                ref={(c) => this._input = c}
+                type="range"
+                min="1"
+                max="10"
+                step="1"
+                defaultValue="1"
+                onChange={this._handleOnChange.bind(this)}
+              />
+            </Panel>
           </div>
         </div>
 
         <div className="row">
           <div className="col-lg-12">
-            <div className="panel panel-default">
-              <div className="panel-heading">
-                <h3 className="panel-title">
-                  {`Investment portfolio`}
-                </h3>
-              </div>
-              <div className="panel-body">
-                <DonutChart currentChart={this.state.currentChart - 1}/>
-              </div>
-            </div>
+            <Panel title={`Investment portfolio`}>
+              <DonutChart currentChart={this.state.currentChart - 1}/>
+            </Panel>
           </div>
         </div>
       </div>
